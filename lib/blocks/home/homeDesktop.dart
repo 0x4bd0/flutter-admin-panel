3e3a2338c42e00562d1e941f8e483921bca42942
abdelkader MOUDJAR <abdoumjr@gmail.com>
2021-03-13 16:44:29 +0100

    fix desktop scrolling issue

--- lib/blocks/home/homeDesktop.dart
+++ lib/blocks/home/homeDesktop.dart
@@ -17,52 +17,69 @@ class HomeDesktop extends StatelessWidget {
         title: const Text('Flutter admin panel',style: TextStyle(color: Colors.black),),
         elevation: 0,
       ),
-      body: SafeArea(child: Container(
-          child : Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Flexible( flex:2,child: Container(constraints: BoxConstraints(maxWidth: 500, minWidth: 300), child: MobileDrawer())),
-              Flexible(
-                flex:8,
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
-                  child: SizedBox.expand(
-                    child: Column(
-                      children: [
-                        Wrap(
-                          direction: Axis.horizontal,
-                          alignment: WrapAlignment.center,
-                          spacing:  20,
-                          runSpacing: 20,
-                          children: [
-                            for (var item in cards)       
-                                  AdminCard(item),
-                          ],
+      body: CustomScrollView(
+        slivers: [
+          SliverFillRemaining(
+          child: SafeArea(child: Container(
+              child : Row(
+                mainAxisSize :MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Flexible( flex:2,child: Container(constraints: BoxConstraints(maxWidth: 500, minWidth: 300), child: MobileDrawer())),
+                  Flexible(
+                    flex:8,
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                      child: SizedBox.expand(
+                        child: SingleChildScrollView(
+                          child: ConstrainedBox(
+                          constraints: BoxConstraints(maxHeight: MediaQuery.of(context).size.height),
+                            child: Column(
+                              children: [
+                                Wrap(
+                                  direction: Axis.horizontal,
+                                  alignment: WrapAlignment.center,
+                                  spacing:  20,
+                                  runSpacing: 20,
+                                  children: [
+                                    for (var item in cards)       
+                                          AdminCard(item),
+                                  ],
+                                ),
+                               Flexible(child: Padding(
+                                 padding: const EdgeInsets.all(10.0),
+                                 child: Row(
+                                   children: [
+                                     Flexible(flex:1,child: Card(child: Container(
+                                       height: MediaQuery.of(context).size.height * 0.5,
+                                       child: Padding(
+                                         padding: const EdgeInsets.all(8.0),
+                                         child: SimpleLineChart(),
+                                       ),
+                                     ))),
+                                    Flexible(flex:1,child: Card(child: Container(
+                                    height: MediaQuery.of(context).size.height * 0.5,
+                                      child: Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: GroupedBarChart(),
+                                      ),
+                                    ))),
+                                   ],
+                                 ),
+                               )),
+                              ],
+                            ),
+                          ),
                         ),
-                       Flexible(child: Padding(
-                         padding: const EdgeInsets.all(10.0),
-                         child: Row(
-                           children: [
-                             Flexible(flex:1,child: Card(child: Padding(
-                               padding: const EdgeInsets.all(8.0),
-                               child: SimpleLineChart(),
-                             ))),
-                            Flexible(flex:1,child: Card(child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: GroupedBarChart(),
-                            ))),
-                           ],
-                         ),
-                       )),
-                      ],
+                      ),
                     ),
                   ),
-                ),
-              ),
-            ],
-          )
-      )),
+                ],
+              )
+          )),
+        )]
+      ),
     );
   }
 }
