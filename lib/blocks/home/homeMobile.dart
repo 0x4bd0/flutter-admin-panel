d702f8386941a7d9c2117ac8747bdb1c8ab1e241
abdelkader MOUDJAR <abdoumjr@gmail.com>
2021-03-13 16:06:29 +0100

    added responsive for charts on mobile

--- lib/blocks/home/homeMobile.dart
+++ lib/blocks/home/homeMobile.dart
@@ -1,6 +1,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_admin_panel/widgets/card.dart';
+import 'package:flutter_admin_panel/widgets/chartOne.dart';
+import 'package:flutter_admin_panel/widgets/chartTwo.dart';
 import 'package:flutter_admin_panel/widgets/drawer.dart';
 
 class HomeMobile extends StatelessWidget {
@@ -16,22 +18,40 @@ class HomeMobile extends StatelessWidget {
       ),
       drawer:  MobileDrawer(),
       body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(10),
-          child: SafeArea(child: Container(
-              child :     Center(
-                  child: Wrap(
-                    direction: Axis.horizontal,
-                    spacing:  20,
-                    runSpacing: 20,
-                    alignment: WrapAlignment.center,
-                    children: [
-                      for (var item in cards)       
-                            AdminCard(item),
-                    ],
-                  ),
-                )
-          )),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: SafeArea(child: Container(
+                  child : Center(
+                      child: Column(
+                        children: [
+                          Wrap(
+                            direction: Axis.horizontal,
+                            spacing:  20,
+                            runSpacing: 20,
+                            alignment: WrapAlignment.center,
+                            children: [
+                              for (var item in cards)       
+                                    AdminCard(item),
+                            ],
+                          ),
+                          SizedBox(height: 20),
+                            Card(child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Container(height:300, child: SimpleLineChart()),
+                            )),
+                           SizedBox(height: 20),
+                             Card(child: Padding(
+                               padding: const EdgeInsets.all(8.0),
+                               child: Container(height:300, child: GroupedBarChart()),
+                             )),
+                        ],
+                      ),
+                    )
+              )),
+            ),
+          ],
         ),
       ),
     );
